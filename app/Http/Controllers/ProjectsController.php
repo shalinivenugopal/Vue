27d6086778c1fd86a8dfad0a817475d7dc2d85cc
Shalinivenugopal <shalinikamini29@gmail.com>
2019-06-12 14:54:22 +0530

    first commit

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    /**
+     * Show the page to create a new project.
+     */
+    public function create()
+    {
+        return view('projects.create', [
+            'projects' => Project::all() 
+        ]);
+    }
+
+    /**
+     * Store a new project in the database.
+     */
+    public function store()
+    {
+        $this->validate(request(), [
+            'name' => 'required',
+            "description" => 'required',
+            "date" => 'required'
+
+        ]); 
+
+        Project::forceCreate([
+            'name' => request('name'),
+            'description' => request('description'),
+            'date' => request('date')
+        ]); 
+
+        return ['message' => 'Project Created!'];
+    }
+}
