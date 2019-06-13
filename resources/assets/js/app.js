7bf0ea9227678e2d159a58a11346d34350dda64d
Shalinivenugopal <shalinikamini29@gmail.com>
2019-06-13 12:18:09 +0530

    Custom Input Components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,10 @@
-alert('app.js');
+import Vue from 'vue';
+import Notification from './components/Notification.vue';
+
+
+new Vue({
+	el : '#app',
+
+
+	components: { Notification }
+}) 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <h1>Laravel</h1>
     </head>
     <body>
          <div id="app">
-            <ul>
-                <li v-for="skill in skills" v-text="skill"></li>
-            </ul>
-             
+<!--             <input type="text" :value="coupon" @input="coupon = $event.target.value">
+-->               <coupon  v-model="coupon">  
          </div>
 
-        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-         <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-         <script src="/js/app.js"></script>
+<!--         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+ -->         <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
+         <script src="./js/app.js"></script>
      </body> 
 </html>
